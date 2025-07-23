199642a0733c7bc56929e40f81fee4ca2b79fbc9
basarsy <basarsoyturk03@gmail.com>
2025-07-23 14:40:23 +0300

    Added ProductController

---
+++ Controllers/ProductController.cs
@@ -0,0 +1,24 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace MyMicroservice.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+
+public class ProductController : Controller
+{
+    public static List<Product> Products { get; set; }= new();
+    
+    [HttpPost]
+    public IActionResult Create(Product product)
+    {
+        Products.Add(product);
+        return Ok(new { message = "Product is successfully created." });
+    }
+    
+    [HttpGet]
+    public IActionResult Get()
+    {
+        return Ok(Products);
+    }
+}
